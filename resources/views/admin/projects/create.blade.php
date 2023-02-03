64d04258bf7661b2f4113e1feaf384a3c56dc3c2
SimoVisc <simonaviscardivs@gmail.com>
2023-02-03 15:35:15 +0100

    Aggiunge type in create

--- resources/views/admin/projects/create.blade.php
+++ resources/views/admin/projects/create.blade.php
@@ -27,6 +27,16 @@
                         <input type="file" class="form-control" id="cover_image" name="cover_image"
                             value="{{ old('cover_image') }}">
                     </div>
+                    <div class="mb-3">
+                        <label for="type_id" class="form-label">Type</label>
+                        <select class="form-select" name="type_id" id="type_id">
+                            <option value="">No type</option>
+                            @foreach ($types as $type)
+                                <option value="{{ $type->id }}" {{ old('type_id') == $type->id ? 'selected' : '' }}>
+                                    {{ $type->name }}</option>
+                            @endforeach
+                        </select>
+                    </div>
                     <button type="submit" class="btn btn-primary">Create</button>
                 </form>
             </div>
