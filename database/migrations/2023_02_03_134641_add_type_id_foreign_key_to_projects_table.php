958b4540edddc879046fba7530c33a7c24e3d734
SimoVisc <simonaviscardivs@gmail.com>
2023-02-03 14:55:23 +0100

    Aggiungo chiave primaria con migration

---
+++ database/migrations/2023_02_03_134641_add_type_id_foreign_key_to_projects_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->foreigId('type_id')->nullable()->constrained()->onDelate('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('projects', function (Blueprint $table) {
+            $table->dropForeig(['type_id']);
+            $table->dropColumn('type_id');
+        });
+    }
+};
