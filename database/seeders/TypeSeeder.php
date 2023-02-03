cba24bb73a9c1b81bf94856515020eeeadb1fbf6
SimoVisc <simonaviscardivs@gmail.com>
2023-02-03 14:43:32 +0100

    Aggiungo seeder

---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Type;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        type::truncate();
+
+        $types = [
+            'Business implementation',
+            'Foundational (business improvement)',
+            'IT infrastructure improvement',
+            'Physical engineering/construction',
+            'Physical infrastructure improvement',
+            'Procurement',
+            'Regulatory/compliance',
+            'Research and Development (R&D)',
+            'Service development',
+            'Transformation/reengineering'
+        ];
+        
+
+        foreach ($types as $type) {
+            $new_type = new Type();
+            $new_type ->name= $type;
+            $new_type ->slug= Str::slug($new_type->name);
+            $new_type ->save();
+        }
+    }
+}
