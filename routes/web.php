75d8a104cdf0ded65d677b82f88ef1bafee2de2f
SimoVisc <simonaviscardivs@gmail.com>
2023-02-03 14:33:53 +0100

    Aggiunge la rotta di type

--- routes/web.php
+++ routes/web.php
@@ -27,6 +27,7 @@ Route::middleware('auth')->prefix('admin')->name('admin.')->group(function () {
     })->name('dashboard');
 
     Route::resource('projects', ProjectController::class)->parameters(['projects' => 'project:slug']);
+    Route::resource('types', TypeController::class)->parameters(['types' => 'type:slug']);
 });
 
 
